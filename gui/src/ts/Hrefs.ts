3e6c35e415828643c69ad0506d36503d34ee6cfa
Minor Gordon <gordom6@rpi.edu>
2020-07-10 13:06:15 -0400

    first cut of Hrefs

---
+++ gui/src/ts/Hrefs.ts
@@ -0,0 +1,40 @@
+const encodeId = (kwds: {id: string, idEncoded?: boolean}) =>
+    kwds.idEncoded ? kwds.id : encodeURIComponent(kwds.id);
+
+class SubHrefs {
+  constructor(readonly home: string) {
+  }
+
+  toString() {
+    return this.home;
+  }
+}
+
+class SchemaHrefs extends SubHrefs {
+}
+
+class SchemasHrefs extends SubHrefs {
+  schema(kwds: { id: string, idEncoded?: boolean}) {
+    return new SchemaHrefs(this.home + encodeId(kwds));
+  }
+}
+
+class SdfDocumentHrefs extends SubHrefs {
+  get schemas() {
+    return new SchemasHrefs(this.home + "schemas/");
+  }
+}
+
+class SdfDocumentsHrefs extends SubHrefs {
+  sdfDocument(kwds: {id: string, idEncoded?: boolean}) {
+    return new SdfDocumentHrefs(this.home + encodeId(kwds));
+  }
+}
+
+export class Hrefs {
+  static readonly contact = "mailto:gordom6@rpi.edu";
+  static readonly gitHub = "https://github.com/tetherless-world/mcs-portal";
+  static readonly home = "/";
+  static readonly schemas = new SchemasHrefs("/schemas/")
+  static readonly sdfDocuments = new SdfDocumentsHrefs("/sdfdocuments/");
+}
