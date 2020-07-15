ab9061c39a2429068c91d6994bde443fa52c2c00
Minor Gordon <gordom6@rpi.edu>
2020-07-15 12:18:49 -0400

    refining SdfDocumentsPage

--- gui/src/ts/components/sdfDocument/SdfDocumentsPage.tsx
+++ gui/src/ts/components/sdfDocument/SdfDocumentsPage.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
 import * as SdfDocumentsPageQueryDocument from "api/queries/SdfDocumentsPageQuery.graphql";
 import {useQuery} from "@apollo/react-hooks";
-import {Card, CardContent, CardHeader, Grid} from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 import {SdfDocumentsPageQuery} from "api/queries/types/SdfDocumentsPageQuery";
 import {Frame} from "components/frame/Frame";
 import {SdfDocumentCard} from "components/sdfDocument/SdfDocumentCard";
@@ -12,20 +18,22 @@ export const SdfDocumentsPage: React.FunctionComponent = () => {
   return (
     <Frame {...query}>
       {({data}) => (
-        <Grid container>
-          <Grid item xs={6}>
-            <Card>
-              <CardHeader title="Schema Data Format Documents" />
-              <CardContent>
-                <Grid container>
-                  {data.sdfDocuments.map((sdfDocument) => (
-                    <Grid item key={sdfDocument.id}>
-                      <SdfDocumentCard {...sdfDocument} />
-                    </Grid>
-                  ))}
-                </Grid>
-              </CardContent>
-            </Card>
+        <Grid container justify="center">
+          <Grid item>
+            <Grid container direction="column" spacing={4}>
+              <Grid item>
+                <Typography variant="h4" style={{textAlign: "center"}}>
+                  Schema Data Format Documents
+                </Typography>
+              </Grid>
+              <Grid item container>
+                {data.sdfDocuments.map((sdfDocument) => (
+                  <Grid item key={sdfDocument.id}>
+                    <SdfDocumentCard {...sdfDocument} />
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       )}
