e5604c4277272d6425d579c46ee5ac8690e42f98
Minor Gordon <gordom6@rpi.edu>
2020-07-17 15:09:35 -0400

    only comment is an ordered list

--- app/formats/sdf/vocabulary/KairosProperties.scala
+++ app/formats/sdf/vocabulary/KairosProperties.scala
@@ -1,16 +1,30 @@
 package formats.sdf.vocabulary
 
 import edu.rpi.tw.twks.uri.Uri
+import formats.sdf.{MalformedSchemaDataFormatDocumentException, RdfListRdfReader}
+import formats.sdf.RdfListRdfReader.read
 import io.github.tetherlessworld.scena.PropertyGetters
 import org.apache.jena.datatypes.xsd.XSDDuration
 import org.apache.jena.rdf.model.{Property, Resource}
+import org.apache.jena.vocabulary.RDF
 
 trait KairosProperties extends PropertyGetters {
   private def getPropertyObjectDurations(property: Property) =
     getPropertyObjects(property).filter(_.isLiteral).map(_.asLiteral()).map(_.getValue).filter(_.isInstanceOf[XSDDuration]).map(_.asInstanceOf[XSDDuration])
 
+  private def getPropertyObjectStringList(property: Property): List[String] =
+    getPropertyObjects(property).flatMap(propertyObject => {
+      if (propertyObject.isLiteral) {
+        List(propertyObject.asLiteral().getString)
+      } else if (propertyObject.isResource) {
+        RdfListRdfReader.read(propertyObject.asResource()).filter(_.isLiteral).map(_.asLiteral().getString)
+      } else {
+        List()
+      }
+    })
+
   def akas: List[String] = getPropertyObjectStrings(KAIROS.aka)
-  def comments: List[String] = getPropertyObjectStrings(KAIROS.comment)
+  def comments: List[String] = getPropertyObjectStringList(KAIROS.comment)
   def entityTypes: List[String] = getPropertyObjectStrings(KAIROS.entityTypes)
   def maxDurations: List[XSDDuration] = getPropertyObjectDurations(KAIROS.maxDuration)
   def minDurations: List[XSDDuration] = getPropertyObjectDurations(KAIROS.minDuration)
