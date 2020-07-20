94423fdc1d8ed2665c3e090199f0bcd4ee2179c7
Minor Gordon <gordom6@rpi.edu>
2020-07-20 11:20:58 -0400

    reflect into GraphQL API

--- app/models/graphql/GraphQlSchemaDefinition.scala
+++ app/models/graphql/GraphQlSchemaDefinition.scala
@@ -2,20 +2,44 @@ package models.graphql
 
 import formats.sdf.SdfDocument
 import io.github.tetherlessworld.twxplore.lib.base.models.graphql.BaseGraphQlSchemaDefinition
-import models.schema.{Duration, EntityType, Slot, Step}
-import sangria.schema.{Argument, Field, ListType, ObjectType, OptionType, Schema, StringType, fields}
+import models.schema.{BeforeAfterStepOrder, ContainerContainedStepOrder, Duration, EntityType, OverlapsStepOrder, Slot, Step, StepOrder, StepOrderFlag}
+import sangria.schema.{Argument, Field, InterfaceType, ListType, ObjectType, OptionType, Schema, StringType, fields}
 import sangria.macros.derive._
 
 object GraphQlSchemaDefinition extends BaseGraphQlSchemaDefinition {
   // Scalar arguments
   val IdArgument = Argument("id", UriType)
 
+  // Enum types
+  implicit val EntityTypeEnumType = deriveEnumType[EntityType]()
+  implicit val StepOrderFlagEnumType = deriveEnumType[StepOrderFlag]()
+
+  // Interface types
+  val StepOrderInterfaceType: InterfaceType[GraphQlSchemaContext, StepOrder] =
+    InterfaceType(
+      "StepOrder",
+      fields[GraphQlSchemaContext, StepOrder](
+        Field("comments", OptionType(ListType(StringType)), resolve = _.value.comments),
+        Field("flags", OptionType(ListType(StepOrderFlagEnumType)), resolve = _.value.flags),
+      )
+    )
+
   // Object types
   implicit val DurationObjectType = deriveObjectType[GraphQlSchemaContext, Duration]()
-  implicit val EntityTypeEnumType = deriveEnumType[EntityType]()
   implicit val SlotObjectType = deriveObjectType[GraphQlSchemaContext, Slot]()
+  implicit val BeforeAfterStepOrderObjectType = deriveObjectType[GraphQlSchemaContext, BeforeAfterStepOrder](
+    Interfaces(StepOrderInterfaceType)
+  )
+  implicit val ContainerContainedStepOrderObjectType = deriveObjectType[GraphQlSchemaContext, ContainerContainedStepOrder](
+    Interfaces(StepOrderInterfaceType)
+  )
+  implicit val OverlapsStepOrderObjectType = deriveObjectType[GraphQlSchemaContext, OverlapsStepOrder](
+    Interfaces(StepOrderInterfaceType)
+  )
   implicit val StepObjectType = deriveObjectType[GraphQlSchemaContext, Step]()
-  implicit val SchemaObjectType = deriveObjectType[GraphQlSchemaContext, models.schema.Schema]()
+  implicit val SchemaObjectType = deriveObjectType[GraphQlSchemaContext, models.schema.Schema](
+    ReplaceField("order", Field("order", ListType(StepOrderInterfaceType), resolve = _.value.order))
+  )
   implicit val SdfDocumentObjectType = deriveObjectType[GraphQlSchemaContext, SdfDocument](
     AddFields(
       Field("name", StringType, resolve = ctx => {
