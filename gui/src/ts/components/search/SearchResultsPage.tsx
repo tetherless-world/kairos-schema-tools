da8ef9f7917a759d37338853232d46ea25678a55
Minor Gordon <gordom6@rpi.edu>
2020-07-27 13:56:16 -0400

    stub SearchResultsPage + use-query-params

---
+++ gui/src/ts/components/search/SearchResultsPage.tsx
@@ -0,0 +1,22 @@
+import {useQueryParam} from "use-query-params";
+import {useQuery} from "@apollo/react-hooks";
+import {SdfDocumentPageQuery} from "api/queries/types/SdfDocumentPageQuery";
+import * as SdfDocumentPageQueryDocument from "api/queries/SdfDocumentPageQuery.graphql";
+import {Frame} from "components/frame/Frame";
+import * as React from "react";
+
+export const SearchResultsPage: React.FunctionComponent = () => {
+  const [queryText] = useQueryParam<string>("query");
+
+  const query = useQuery<SdfDocumentPageQuery>(SdfDocumentPageQueryDocument, {
+    variables: {text: queryText},
+  });
+
+  return (
+    <Frame {...query}>
+      {({data}) => {
+        return <div></div>;
+      }}
+    </Frame>
+  );
+};
