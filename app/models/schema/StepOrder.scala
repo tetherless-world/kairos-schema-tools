c880a06817ebfd9ef01b2f08da375bc546d661da
Minor Gordon <gordom6@rpi.edu>
2020-08-04 11:31:02 -0400

    make StepOrder a sealed trait

--- app/models/schema/StepOrder.scala
+++ app/models/schema/StepOrder.scala
@@ -2,8 +2,34 @@ package models.schema
 
 import edu.rpi.tw.twks.uri.Uri
 
-trait StepOrder {
+sealed trait StepOrder {
   val comments: Option[List[String]]
   val flags: Option[List[StepOrderFlag]]
   def stepIds: List[Uri]
+}
+
+final case class BeforeAfterStepOrder(
+                                       after: List[Uri],
+                                       before: List[Uri],
+                                       comments: Option[List[String]],
+                                       flags: Option[List[StepOrderFlag]]
+                                     ) extends StepOrder {
+  final override def stepIds: List[Uri] = after ++ before
+}
+
+final case class ContainerContainedStepOrder(
+                                              comments: Option[List[String]],
+                                              contained: List[Uri],
+                                              container: Uri,
+                                              flags: Option[List[StepOrderFlag]]
+                                            ) extends StepOrder {
+  final override def stepIds: List[Uri] = contained :+ container
+}
+
+final case class OverlapsStepOrder(
+                                    comments: Option[List[String]],
+                                    flags: Option[List[StepOrderFlag]],
+                                    overlaps: List[Uri]
+                                  ) extends StepOrder {
+  final override def stepIds: List[Uri] = overlaps
 }
