ff278ddd7bee596c4447d08e7b99d50919727d42
Minor Gordon <gordom6@rpi.edu>
2020-08-11 17:53:35 -0400

    break apart SchemaTableOfContents into multiple components

--- gui/src/ts/components/schema/SchemaTableOfContents.tsx
+++ gui/src/ts/components/schema/SchemaTableOfContents.tsx
@@ -16,25 +16,150 @@ import {SdfDocumentSourceLink} from "components/link/SdfDocumentSourceLink";
 
 const useStyles = makeStyles((theme) => ({
   nestedListItem: {
-    paddingLeft: theme.spacing(12),
+    paddingLeft: theme.spacing(4),
   },
 }));
+
+interface SchemaTableOfContentsSlot {
+  id: string;
+  roleName: string;
+}
+
+interface SchemaTableOfContentsStep {
+  id: string;
+  name: string;
+  participants: readonly SchemaTableOfContentsStepParticipant[] | null;
+}
+
+interface SchemaTableOfContentsStepParticipant {
+  id: string;
+  name: string;
+}
 
 export const SchemaTableOfContents: React.FunctionComponent<{
   hrefs: SchemaHrefs;
   includeSourceLinks?: boolean;
   schema: {
     id: string;
-    slots: readonly {id: string; roleName: string}[];
+    slots: readonly SchemaTableOfContentsSlot[];
     sdfDocumentId: string;
-    steps: readonly {
-      id: string;
-      name: string;
-      participants: readonly {id: string; name: string}[] | null;
-    }[];
+    steps: readonly SchemaTableOfContentsStep[];
   };
 }> = ({hrefs, includeSourceLinks, schema}) => {
   const classes = useStyles();
+
+  const SlotsList: React.FunctionComponent<{
+    slots: readonly SchemaTableOfContentsSlot[];
+  }> = ({slots}) => (
+    <List component="div" disablePadding>
+      {slots.map((slot) => (
+        <ListItem className={classes.nestedListItem} key={slot.id}>
+          <ListItemIcon>
+            <WorkIcon />
+          </ListItemIcon>
+          <ListItemText>
+            <Grid container spacing={2} style={{alignItems: "center"}}>
+              <Grid item>
+                <Link to={hrefs.slot({id: slot.id})}>
+                  Slot: {slot.roleName}
+                </Link>
+              </Grid>
+              {includeSourceLinks ? (
+                <Grid item>
+                  <SdfDocumentSourceLink
+                    to={{
+                      schemaId: schema.id,
+                      sdfDocumentId: schema.sdfDocumentId,
+                      slotId: slot.id,
+                    }}
+                  />
+                </Grid>
+              ) : null}
+            </Grid>
+          </ListItemText>
+        </ListItem>
+      ))}
+    </List>
+  );
+
+  const StepParticipantsList: React.FunctionComponent<{
+    participants: readonly SchemaTableOfContentsStepParticipant[];
+    stepId: string;
+  }> = ({participants, stepId}) => (
+    <List component="div" disablePadding>
+      {participants.map((participant) => (
+        <ListItem className={classes.nestedListItem} key={participant.id}>
+          <ListItemIcon>
+            <WorkIcon />
+          </ListItemIcon>
+          <ListItemText>
+            <Grid container spacing={2} style={{alignItems: "center"}}>
+              <Grid item>
+                <Link to={hrefs.step({id: stepId})}>
+                  Participant: {participant.name}
+                </Link>
+              </Grid>
+              {includeSourceLinks ? (
+                <Grid item>
+                  <SdfDocumentSourceLink
+                    to={{
+                      schemaId: schema.id,
+                      sdfDocumentId: schema.sdfDocumentId,
+                      stepId: stepId,
+                      stepParticipantId: participant.id,
+                    }}
+                  />
+                </Grid>
+              ) : null}
+            </Grid>
+          </ListItemText>
+        </ListItem>
+      ))}
+    </List>
+  );
+
+  const StepsList: React.FunctionComponent<{
+    steps: readonly SchemaTableOfContentsStep[];
+  }> = ({steps}) => (
+    <List component="div" disablePadding>
+      {schema.steps.map((step) => (
+        <ListItem className={classes.nestedListItem} key={step.id}>
+          <ListItemText>
+            <Grid container direction="column">
+              <Grid item>
+                <Grid container spacing={2} style={{alignItems: "center"}}>
+                  <Grid item>
+                    <Link to={hrefs.step({id: step.id})}>
+                      Step: {step.name}
+                    </Link>
+                  </Grid>
+                  {includeSourceLinks ? (
+                    <Grid item>
+                      <SdfDocumentSourceLink
+                        to={{
+                          schemaId: schema.id,
+                          sdfDocumentId: schema.sdfDocumentId,
+                          stepId: step.id,
+                        }}
+                      />
+                    </Grid>
+                  ) : null}
+                </Grid>
+              </Grid>
+              {step.participants ? (
+                <Grid item>
+                  <StepParticipantsList
+                    participants={step.participants}
+                    stepId={step.id}
+                  />
+                </Grid>
+              ) : null}
+            </Grid>
+          </ListItemText>
+        </ListItem>
+      ))}
+    </List>
+  );
 
   return (
     <List data-cy="schema-toc">
@@ -53,82 +178,11 @@ export const SchemaTableOfContents: React.FunctionComponent<{
               </Link>
             </ListItemText>
           </ListItem>
-          {schemaSection.id === "slots" || schemaSection.id === "steps" ? (
-            <List component="div" disablePadding>
-              {schemaSection.id === "slots"
-                ? schema.slots.map((slot) => (
-                    <ListItem className={classes.nestedListItem} key={slot.id}>
-                      <ListItemIcon>
-                        <WorkIcon />
-                      </ListItemIcon>
-                      <ListItemText>
-                        <Grid
-                          container
-                          spacing={2}
-                          style={{alignItems: "center"}}
-                        >
-                          <Grid item>
-                            <Link to={hrefs.slot({id: slot.id})}>
-                              Slot: {slot.roleName}
-                            </Link>
-                          </Grid>
-                          {includeSourceLinks ? (
-                            <Grid item>
-                              <SdfDocumentSourceLink
-                                to={{
-                                  schemaId: schema.id,
-                                  sdfDocumentId: schema.sdfDocumentId,
-                                  slotId: slot.id,
-                                }}
-                              />
-                            </Grid>
-                          ) : null}
-                        </Grid>
-                      </ListItemText>
-                    </ListItem>
-                  ))
-                : null}
-              {schemaSection.id === "steps"
-                ? schema.steps.map((step) => (
-                    <ListItem className={classes.nestedListItem} key={step.id}>
-                      <ListItemIcon>
-                        <WorkIcon />
-                      </ListItemIcon>
-                      <ListItemText>
-                        <Grid container direction="column">
-                          <Grid item>
-                            <Grid
-                              container
-                              spacing={2}
-                              style={{alignItems: "center"}}
-                            >
-                              <Grid item>
-                                <Link to={hrefs.step({id: step.id})}>
-                                  Step: {step.name}
-                                </Link>
-                              </Grid>
-                              {includeSourceLinks ? (
-                                <Grid item>
-                                  <SdfDocumentSourceLink
-                                    to={{
-                                      schemaId: schema.id,
-                                      sdfDocumentId: schema.sdfDocumentId,
-                                      stepId: step.id,
-                                    }}
-                                  />
-                                </Grid>
-                              ) : null}
-                            </Grid>
-                          </Grid>
-                          {step.participants ? (
-                            <Grid item>Step participants</Grid>
-                          ) : null}
-                        </Grid>
-                      </ListItemText>
-                    </ListItem>
-                  ))
-                : null}
-            </List>
+          {schemaSection.id === "slots" ? (
+            <SlotsList slots={schema.slots} />
+          ) : null}
+          {schemaSection.id === "steps" ? (
+            <StepsList steps={schema.steps} />
           ) : null}
         </React.Fragment>
       ))}
