4c1b24a4abda750a83a79118d9ac4712dc34d1f8
Minor Gordon <gordom6@rpi.edu>
2020-08-17 17:34:56 -0400

    SdfDocumentPath builder

--- app/models/schema/SdfDocumentPath.scala
+++ app/models/schema/SdfDocumentPath.scala
@@ -2,33 +2,49 @@ package models.schema
 
 import edu.rpi.tw.twks.uri.Uri
 
-final case class SdfDocumentPath
+final case class SdfDocumentPathPrimitive(id: Uri, slot: Option[SdfDocumentPathPrimitiveSlot]) {}
 
-final case class SdfDocumentPathStep(id: Uri)
+final case class SdfDocumentPathPrimitiveSlot(id: Uri)
 
-final case class SdfDocumentPathSchema(id: Uri)
+final case class SdfDocumentPathStep(id: Uri, participant: Option[SdfDocumentPathStepParticipant])
 
-final case class SdfDocumentPathSdfDocument(id: Uri, primitive: Option[SdfDocumentPathPrimitive], schema: Option[SdfDocumentPathSchema])
+final case class SdfDocumentPathStepParticipant(id: Uri)
 
-final case class SdfDocumentPath(
-                                  sdfDocument: Uri,
-                                  primitiveId: Option[Uri] = None,
-                                  primitiveSlotId: Option[Uri] = None,
-                                  stepId: Option[Uri] = None,
-                                  stepParticipantId: Option[Uri] = None,
-                                  schemaId: Option[Uri] = None,
-                                  schemaSlotId: Option[Uri] = None,
-                                ) {
-  if (primitiveSlotId.isDefined && !primitiveId.isDefined) {
-    throw new IllegalArgumentException("primitive id must be defined if the primitive slot id is defined")
+final case class SdfDocumentPathSchema(id: Uri, slot: Option[SdfDocumentPathSchemaSlot], step: Option[SdfDocumentPathStep]) {
+  if (slot.isDefined && step.isDefined) {
+    throw new IllegalArgumentException("may only specify one of slot or step")
   }
-  if (schemaSlotId.isDefined && !schemaId.isDefined) {
-    throw new IllegalArgumentException("schema id must be defined if the schema slot id is defined")
+}
+
+final case class SdfDocumentPathSchemaSlot(id: Uri)
+
+final case class SdfDocumentPath(id: Uri, primitive: Option[SdfDocumentPathPrimitive], schema: Option[SdfDocumentPathSchema]) {
+  if (primitive.isDefined && schema.isDefined) {
+    throw new IllegalArgumentException("may only specify one of primitive or schema")
   }
-  if (stepId.isDefined && !schemaId.isDefined) {
-    throw new IllegalArgumentException("schema id must be defined if the step id is defined")
+}
+
+object SdfDocumentPath {
+  class SdfDocumentPathBuilder(sdfDocumentId: Uri) {
+    class SdfDocumentPathPrimitiveBuilder(primitiveId: Uri) {
+      def build = SdfDocumentPath(id = sdfDocumentId, primitive = Some(SdfDocumentPathPrimitive(id = primitiveId, slot = None)), schema = None)
+      def slot(id: Uri) = SdfDocumentPath(id = sdfDocumentId, primitive = Some(SdfDocumentPathPrimitive(id = primitiveId, slot = Some(SdfDocumentPathPrimitiveSlot(id = id)))), schema = None)
+    }
+
+    class SdfDocumentPathSchemaBuilder(schemaId: Uri) {
+      class SdfDocumentPathSchemaStepBuilder(stepId: Uri) {
+        def build = SdfDocumentPath(id = sdfDocumentId, primitive = None, schema = Some(SdfDocumentPathSchema(id = schemaId, slot = None, step = Some(SdfDocumentPathStep(id = stepId, participant = None)))))
+        def participant(id: Uri) = SdfDocumentPath(id = sdfDocumentId, primitive = None, schema = Some(SdfDocumentPathSchema(id = schemaId, slot = None, step = Some(SdfDocumentPathStep(id = stepId, participant = Some(SdfDocumentPathStepParticipant(id = id)))))))
+      }
+
+      def build = SdfDocumentPath(id = sdfDocumentId, primitive = None, schema = Some(SdfDocumentPathSchema(id = schemaId, slot = None, step = None)))
+      def slot(id: Uri) = SdfDocumentPath(id = sdfDocumentId, primitive = None, schema = Some(SdfDocumentPathSchema(id = schemaId, slot = Some(SdfDocumentPathSchemaSlot(id = id)), step = None)))
+      def step(id: Uri) = new SdfDocumentPathSchemaStepBuilder(stepId = id)
+    }
+
+    def build = SdfDocumentPath(id = sdfDocumentId, primitive = None, schema = None)
+    def schema(id: Uri) = new SdfDocumentPathSchemaBuilder(schemaId = id)
   }
-  if (stepParticipantId.isDefined && !stepId.isDefined) {
-    throw new IllegalArgumentException("step id must be defined if the step participant id is defined")
-  }
+
+  def builder(id: Uri) = new SdfDocumentPathBuilder(id)
 }
