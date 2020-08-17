f919ef5ef45cddb43aa371a8462fadc7b4e7a814
Minor Gordon <gordom6@rpi.edu>
2020-08-17 16:54:26 -0400

    Schema has no super

--- app/models/schema/Schema.scala
+++ app/models/schema/Schema.scala
@@ -4,22 +4,21 @@ import edu.rpi.tw.twks.uri.Uri
 import models.json.JsonNodeLocation
 
 case class Schema(
-  aka: Option[List[String]],
-  comments: Option[List[String]],
-  description: String,
-  entityRelations: List[EntityRelation],
-  id: Uri,
-  name: String,
-  order: List[StepOrder],
-  references: Option[List[String]],
-  sdfDocumentId: Uri,
-  slots: List[Slot],
-  sourceJsonNodeLocation: JsonNodeLocation,
-  steps: List[Step],
-  `super`: Option[Uri],
-  ta2: Boolean,
-  template: Option[String],
-  version: String
+                   aka: Option[List[String]],
+                   comments: Option[List[String]],
+                   description: String,
+                   entityRelations: List[EntityRelation],
+                   id: Uri,
+                   name: String,
+                   order: List[StepOrder],
+                   references: Option[List[String]],
+                   sdfDocumentId: Uri,
+                   slots: List[SchemaSlot],
+                   sourceJsonNodeLocation: JsonNodeLocation,
+                   steps: List[Step],
+                   ta2: Boolean,
+                   template: Option[String],
+                   version: String
 ) {
   def label = name
 }
