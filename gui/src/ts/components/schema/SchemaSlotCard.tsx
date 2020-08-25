48a5d810eb67d994d6d1338bb596ba6548214d28
Minor Gordon <gordom6@rpi.edu>
2020-08-25 16:07:51 -0400

    collapse some components

--- gui/src/ts/components/schema/SchemaSlotCard.tsx
+++ gui/src/ts/components/schema/SchemaSlotCard.tsx
@@ -1,7 +1,14 @@
 import {SchemaPageQuery_schemaById_slots} from "api/queries/types/SchemaPageQuery";
-import {Card, CardContent, CardHeader} from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Table,
+  TableBody,
+} from "@material-ui/core";
 import * as React from "react";
-import {SlotDetailsTable} from "components/schema/SlotDetailsTable";
+import {StringFieldTableRow} from "components/table/StringFieldTableRow";
+import {StringListFieldTableRow} from "components/table/StringListFieldTableRow";
 
 export const SchemaSlotCard: React.FunctionComponent<{
   slot: SchemaPageQuery_schemaById_slots;
@@ -9,7 +16,33 @@ export const SchemaSlotCard: React.FunctionComponent<{
   <Card>
     <CardHeader title={"Slot: " + slot.id} />
     <CardContent>
-      <SlotDetailsTable slot={slot} />
+      <Table>
+        <TableBody>
+          <StringFieldTableRow name="Identifier" value={slot.id} />
+          <StringListFieldTableRow
+            direction="column"
+            name="Also known as"
+            values={slot.aka}
+          />
+          <StringListFieldTableRow
+            direction="column"
+            name="Comments"
+            values={slot.comments}
+          />
+          <StringListFieldTableRow
+            direction="row"
+            name="Entity types"
+            values={slot.entityTypes}
+          />
+          <StringListFieldTableRow
+            direction="column"
+            name="References"
+            values={slot.references}
+          />
+          <StringFieldTableRow name="Refvar" value={slot.refvar} />
+          <StringFieldTableRow name="Role name" value={slot.roleName} />
+        </TableBody>
+      </Table>
     </CardContent>
   </Card>
 );
