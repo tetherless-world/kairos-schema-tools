abdee58bdb747a2af7d60552719c989eabb6e79e
Minor Gordon <gordom6@rpi.edu>
2020-11-06 17:30:30 -0500

    add entities to refvar table

--- gui/src/ts/components/schema/RefvarTable.tsx
+++ gui/src/ts/components/schema/RefvarTable.tsx
@@ -12,30 +12,53 @@ import {
   TableRow,
 } from "@material-ui/core";
 import {Link} from "components/link/Link";
+import CheckIcon from "@material-ui/icons/Check";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  table: {
+    border: "2px solid !important",
+  },
+  tableCell: {
+    borderLeft: "2px solid !important",
+    textAlign: "center",
+  },
+}));
 
 export const RefvarTable: React.FunctionComponent<{
   hrefs: SchemaHrefs;
   schema: SchemaPageQuery_schemaById;
 }> = ({hrefs, schema}) => {
+  const classes = useStyles();
+
   const refvars = new Set<string>();
-  for (const step of schema.steps.list) {
-    if (!step.participants) {
-      continue;
-    }
-    for (const participant of step.participants) {
-      if (participant.refvar) {
-        refvars.add(participant.refvar);
-      }
+  for (const entity of (schema.entities ?? []).filter(
+    (entity) => !!entity.refvar
+  )) {
+    refvars.add(entity.refvar!);
+  }
+  for (const step of schema.steps.list.filter((step) => !!step.participants)) {
+    for (const participant of step.participants!.filter(
+      (participant) => !!participant.refvar
+    )) {
+      refvars.add(participant.refvar!);
     }
   }
 
   return (
-    <Table>
+    <Table className={classes.table}>
       <TableHead>
         <TableRow>
-          <TableCell>Refvar</TableCell>
+          <TableCell className={classes.tableCell}>Refvar</TableCell>
+          {(schema.entities ?? [])
+            .filter((entity) => !!entity.refvar)
+            .map((entity) => (
+              <TableCell className={classes.tableCell} key={entity.id}>
+                <Link to={hrefs.entity(entity)}>Entity: {entity.name}</Link>
+              </TableCell>
+            ))}
           {schema.steps.list.map((step) => (
-            <TableCell key={step.id}>
+            <TableCell className={classes.tableCell} key={step.id}>
               <Link to={hrefs.step(step)}>Step: {step.label}</Link>
             </TableCell>
           ))}
@@ -45,13 +68,20 @@ export const RefvarTable: React.FunctionComponent<{
         {[...refvars].map((refvar) => {
           return (
             <TableRow key={refvar}>
-              <TableCell>{refvar}</TableCell>
+              <TableCell className={classes.tableCell}>{refvar}</TableCell>
+              {(schema.entities ?? [])
+                .filter((entity) => !!entity.refvar)
+                .map((entity) => (
+                  <TableCell className={classes.tableCell}>
+                    {entity.refvar === refvar ? <CheckIcon /> : null}
+                  </TableCell>
+                ))}
               {schema.steps.list.map((step) => {
                 const participantsWithRefvar = step.participants?.filter(
                   (participant) => participant.refvar === refvar
                 );
                 return (
-                  <TableCell key={step.id}>
+                  <TableCell className={classes.tableCell} key={step.id}>
                     {participantsWithRefvar &&
                     participantsWithRefvar.length > 0 ? (
                       <List>
